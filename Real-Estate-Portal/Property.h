2cb2094c378b28bec2daded9c4a8c4533fb1e913
ghazaly118 <amohahazaly@gmail.com>
2024-04-17 00:52:42 +0200

    property fields

---
+++ Real-Estate-Portal/Property.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+class Property
+{
+	string Location, PropertyType, BuildingNumber;
+	int ApartmentNumber, SquareFootage, NumberOfBedrooms;
+	bool Verified;
+
+
+};
+
