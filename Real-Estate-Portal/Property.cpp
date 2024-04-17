e00eb850162cfdae43643ab9643025aba69a4d70
Khalifuu <khalifa15223011@gmail.com>
2024-04-17 17:34:52 +0200

    Property default Constructor

--- Real-Estate-Portal/Property.cpp
+++ Real-Estate-Portal/Property.cpp
@@ -3,5 +3,8 @@ void Property() {
 	string Location = " ";
 	string PropertyType = " ";
 	string BuildingNumber = " ";
-
+	int ApartmentNumber = 0;
+	int SquareFootage = 0;
+	int NumberOfBedrooms = 0;
+	bool verified = false;
 }
