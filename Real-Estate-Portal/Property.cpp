7415786a4b1e8651ead0c06696dbfc17bfbdf191
midoeltelbany22@gmail.com <midoeltelbany22@gmail.com>
2024-04-17 18:49:32 +0200

    Property setters and getters and constructors

--- Real-Estate-Portal/Property.cpp
+++ Real-Estate-Portal/Property.cpp
@@ -3,5 +3,132 @@ void Property() {
 	string Location = " ";
 	string PropertyType = " ";
 	string BuildingNumber = " ";
+	
+
 
 }
+
+
+                                                                                         //constructors
+
+
+Property::Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, bool verified)
+{
+	this->Location = Location;
+	this->PropertyType = PropertyType;
+	this->BuildingNumber = BuildingNumber;
+	this->ApartmentNumber = ApartmentNumber;
+	this->SquareFootage = SquareFootage;
+	this->NumberOfBedrooms = NumberOfBedrooms;
+	this->Verified = Verified;
+
+}
+Property::Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms)
+{
+	Property(Location, PropertyType, BuildingNumber, ApartmentNumber, SquareFootage, NumberOfBedrooms,false);
+}
+Property::Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage)
+{
+	Property(Location, PropertyType, BuildingNumber, ApartmentNumber, SquareFootage, 0);
+}
+Property::Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber)
+{
+	Property(Location, PropertyType, BuildingNumber, ApartmentNumber, 0);
+}
+Property::Property(string Location, string PropertyType, string BuildingNumber)
+{
+	Property(Location, PropertyType, BuildingNumber, 0);
+}
+Property::Property(string Location, string PropertyType)
+{
+	Property(Location, PropertyType,"");
+}
+Property::Property(string Location)
+{
+	Property(Location,"");
+}
+Property::Property()
+{
+	Property("");
+}
+Property::Property(const Property& Other)
+{
+	this->Location = Other.Location;
+	this->PropertyType = Other.PropertyType;
+	this->BuildingNumber = Other.BuildingNumber;
+	this->ApartmentNumber = Other.ApartmentNumber;
+	this->SquareFootage = Other.SquareFootage;
+	this->NumberOfBedrooms = Other.NumberOfBedrooms;
+	this->Verified = Other.Verified;
+}
+
+
+                                                                                             //setters
+
+
+void Property::SetLocation(string Location)
+{
+	this->Location = Location;
+}
+void Property::SetPropertyType(string PropertyType)
+{
+	this->PropertyType = PropertyType;
+}
+void Property::SetBuildingNumber(string BuildingNumber)
+{
+	this->BuildingNumber = BuildingNumber;
+}
+void Property::SetApartmentNumber(int ApartmentNumber)
+{
+	this->ApartmentNumber = ApartmentNumber;
+}
+void Property::SetSquareFootage(int SquareFootage)
+{
+	this->SquareFootage = SquareFootage;
+}
+void Property::SetNumberOfBedrooms(int NumberOfBedrooms)
+{
+	this->NumberOfBedrooms = NumberOfBedrooms;
+}
+void Property::SetVerified(bool Verified)
+{
+	this->Verified = Verified;
+}
+
+
+                                                                                           //getters
+
+
+string Property::GetLocation()
+{
+	return Location;
+}
+string Property::GetPropertyType()
+{
+	return PropertyType;
+}
+string Property::GetBuildingNumber()
+{
+	return BuildingNumber;
+}
+int Property::GetApartmentNumber()
+{
+	return ApartmentNumber;
+}
+int Property::GetsquareFootage()
+{
+	return SquareFootage;
+}
+int Property::GetNumberOfBedrooms()
+{
+	return NumberOfBedrooms;
+}
+bool Property::GetVerfied()
+{
+	return Verified;
+}
+
+
+
+
+
--- Real-Estate-Portal/Property.h
+++ Real-Estate-Portal/Property.h
@@ -9,7 +9,33 @@ class Property
 	bool Verified;
 
 public:
+
+	Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, bool Verified);
+	Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms);
+	Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage);
+	Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber);
+	Property(string Location, string PropertyType, string BuildingNumber);
+	Property(string Location, string PropertyType);
+	Property(string Location);
+	Property(const Property& Other);
 	Property();
-	Property(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, bool verified);
+
+	void SetLocation(string location);
+	string GetLocation();
+	void SetPropertyType(string PropertyType);
+	string GetPropertyType();
+	void SetBuildingNumber(string BuildingNumber);
+	string GetBuildingNumber();
+	void SetApartmentNumber(int ApartmentNumber);
+	int GetApartmentNumber();
+	void SetSquareFootage(int SquareFootage);
+	int GetsquareFootage();
+	void SetNumberOfBedrooms(int NumberOfBedrooms);
+	int GetNumberOfBedrooms();
+	int GetsquareFootage();
+	void SetVerified(bool Verified);
+	bool GetVerfied();
+
+
 };
 
