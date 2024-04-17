68928cdd525fa41da3d0ad124a9125b79d181745
RamyNasser4 <ramy_rnz@hotmail.com>
2024-04-17 11:44:33 +0000

    System body

--- Real-Estate-Portal/System.cpp
+++ Real-Estate-Portal/System.cpp
@@ -1,1 +1,9 @@
 #include "System.h"
+void System::SignUp(User user) {
+    /*if (users.find(user.id) == users.end()) {
+        users[user.id] = new User(user);
+    }
+    else {
+        cout << "User already exists" << endl;
+    }*/
+}
--- Real-Estate-Portal/System.h
+++ Real-Estate-Portal/System.h
@@ -6,13 +6,6 @@ class System
 {
 	map<int, User> users;
 public:
-	void SignUp(User user) {
-		/*if (users.find(user.id) == users.end()) {
-			users[user.id] = new User(user);
-		}
-		else {
-			cout << "User already exists" << endl;
-		}*/
-	}
+	void SignUp(User user);
 };
 
