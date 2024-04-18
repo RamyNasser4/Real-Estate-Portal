57c32b1c51165f9b0b91adae1ffe8dede85d2074
Ahany42 <ahanyfathy42@gmail.com>
2024-04-18 23:35:25 +0200

    Added Property to HashTables

--- Real-Estate-Portal/Admin.cpp
+++ Real-Estate-Portal/Admin.cpp
@@ -2,6 +2,7 @@
 #include "Property.h"
 #include "User.h"
 #include "System.h"
+#include <string>
 using namespace std;
 Admin::Admin(string firstName, string lastName, int nationalId, string password) : User(firstName, lastName, nationalId, password) {
 
@@ -23,6 +24,11 @@ Admin::Admin(const Admin& admin) : User((User)admin) {
 }
 void Admin::AddProperty(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, bool verified, int price, string currentUserName, int currentUserId) {
 	Property* NewProperty = new Property(Location, PropertyType, BuildingNumber, ApartmentNumber, SquareFootage, NumberOfBedrooms,true,price,currentUserName,currentUserId);
+	string propertyId = ApartmentNumber + " " + BuildingNumber + " " + Location;
+	System::propertyFilterSquareFootage[SquareFootage][propertyId] = NewProperty;
+	System::propertyFilterLocations[Location][propertyId] = NewProperty;
+	System::propertyFilterType[PropertyType][propertyId] = NewProperty;
+	System::propertyFilterBedRooms[NumberOfBedrooms][propertyId] = NewProperty;
 };
 void Admin::RemoveUser(int ID, unordered_map<int, User*>& users) {
 	if (users.find(ID) == users.end()) {
