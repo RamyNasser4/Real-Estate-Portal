5e28191f459f761881bf5d99282ffc8b02cd7cf5
RamyNasser4 <ramy_rnz@hotmail.com>
2024-04-19 18:07:53 +0200

    Read File

--- Real-Estate-Portal/main.cpp
+++ Real-Estate-Portal/main.cpp
@@ -2,21 +2,46 @@
 #include "System.h"
 #include "Admin.h"
 #include "User.h"
+#include <fstream>
+#include <sstream>
 using namespace std;
+void readFile(System* system) {
+	fstream readFile;
+	readFile.open("../users.txt", ios::out | ios::in);
+	if (readFile.is_open()) {
+		string data;
+		while (getline(readFile, data)) {
+			stringstream ss(data);
+			// Declare variables for fields
+			string firstName, lastName, stringId, password;
+			// Split the line using '*' as the delimiter
+			getline(ss, firstName, '*');
+			getline(ss, lastName, '*');
+			getline(ss, stringId, '*');
+			getline(ss, password, '*');
+			int ID = stoi(stringId);
+			system->SignUp(firstName, lastName, ID, password);
+		}
+	}
+	else {
+		cout << "error" << endl;
+	}
+	readFile.close();
+}
 int main() {
 	//test sign up and login
 	System* system = new System();
-	system->SignUp("Ramy", "Khalifa",23,"Ghazaly123");
+	system->SignUp("Ramy", "Khalifa", 23, "Ghazaly123");
 	system->SignUp("Ramy", "Khalifa", 23, "Ghazaly123");
 	system->SignUp("khalifa", "Alawe", 23, "Ghazaly00");
-	system->Login(12,"Koty");
+	system->Login(12, "Koty");
 	system->Login(23, "Ghazaly123");
 	system->Login(23, "abdo");
 	system->Login(15, "Ghazaly123");
-	
+
 	//test user
-	User* user= new User("Sebaay","Ashraf",65,"koty");
-	cout << user->GetFirstName()<<endl;
+	User* user = new User("Sebaay", "Ashraf", 65, "koty");
+	cout << user->GetFirstName() << endl;
 	cout << user->GetLastName() << endl;
 	cout << user->GetName() << endl;
 	cout << user->GetNationalId() << endl;
@@ -36,5 +61,5 @@ int main() {
 	system->RemoveUser(1,23);
 	system->Login(23, "Ghazaly123");
 	system->SignUp("Ramy", "Khalifa", 23, "Ghazaly123");*/
-	
+	readFile(system);
 }
