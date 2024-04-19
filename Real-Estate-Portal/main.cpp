c3a440fc98d41dbd39103aa35f8255fdb874f2ac
Khalifuu <khalifa15223011@gmail.com>
2024-04-19 18:43:38 +0200

    writeFile fn

--- Real-Estate-Portal/main.cpp
+++ Real-Estate-Portal/main.cpp
@@ -28,6 +28,17 @@ void readFile(System* system) {
 	}
 	readFile.close();
 }
+void writeFile(System *system) {
+	fstream writefile ("../users.txt", ios::out | ios::in | ios::app);
+	if (!writefile.is_open()) {
+		cout << "Failed to open the file"<<endl;
+		return;
+	}
+	for (auto it = system->GetUsers().begin(); it != system->GetUsers().end(); ++it) {
+		writefile << it->second->GetFirstName() << "*" << it->second->GetLastName() << "*" << it->second->GetNationalId() << "*" << it->second->GetPassword() << endl;
+	}
+	writefile.close();
+}
 int main() {
 	//test sign up and login
 	System* system = new System();
@@ -62,4 +73,5 @@ int main() {
 	system->Login(23, "Ghazaly123");
 	system->SignUp("Ramy", "Khalifa", 23, "Ghazaly123");*/
 	readFile(system);
+	writeFile(system);
 }
