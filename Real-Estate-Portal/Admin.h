05f865563518dab070220f2f9b44a158ed8f23bc
sebaey124 <abdelrahmansebaay0@gmail.com>
2024-05-04 22:29:53 +0300

    change national id from intger to string

--- Real-Estate-Portal/Admin.h
+++ Real-Estate-Portal/Admin.h
@@ -7,15 +7,15 @@ class Admin : public User
 protected:
 public:
 	Admin();
-	Admin(string firstName, string lastName, int nationalId, string password);
-	Admin(string firstName, string lastName, int nationalId);
+	Admin(string firstName, string lastName, string nationalId, string password);
+	Admin(string firstName, string lastName, string nationalId);
 	Admin(string firstName, string lastName);
 	Admin(string firstName);
 	Admin(const Admin& other);
 
-	void AddProperty(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, int price, string currentUserName, int currentUserId, string propertyDescription, System& system);
-	void EditProperty(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, int price, string currentUserName, int currentUserId, string propertyDescription, System& system, string propertyId);
-	void RemoveUser(int ID, System& system);
+	void AddProperty(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, int price, string currentUserName, string currentUserId, string propertyDescription, System& system);
+	void EditProperty(string Location, string PropertyType, string BuildingNumber, int ApartmentNumber, int SquareFootage, int NumberOfBedrooms, int price, string currentUserName, string currentUserId, string propertyDescription, System& system, string propertyId);
+	void RemoveUser(string ID, System& system);
 	void RemoveProperty(string propertyId,System &system);
 
 	void AdminApproveorDeclineProperty(System &system, bool approve);
